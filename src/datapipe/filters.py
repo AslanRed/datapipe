8fa596236acf080f0fc492611132098e3e67352c
Aslan <aslan.br19@gmail.com>
2026-05-31 15:07:59 +0300

    feat(new filters): added 2 new filters specifc to chosen dataset: filter by brand and filter by year.

--- src/datapipe/filters.py
+++ src/datapipe/filters.py
@@ -13,3 +13,23 @@ def filter_by_emptys(
             continue
         else:
             yield row
+
+
+def filter_by_brand(
+    brand_name: str, func: Generator[list[str], None, None] = read_csv()
+) -> Generator[list[str], None, None]:
+    for row in func:
+        if brand_name == row[1]:
+            yield row
+        else:
+            continue
+
+
+def filter_by_year(
+    product_year: int, func: Generator[list[str], None, None] = read_csv()
+) -> Generator[list[str], None, None]:
+    for row in func:
+        if product_year == row[2]:
+            yield row
+        else:
+            continue
